1fe588381fbf4ff991639e04a20c1b5c4a90b069
Thomas Deconinck <thomas.d@colisweb.com>
2018-11-10 20:00:01 +0100

    clean

--- src/services/firebase/data/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyDixCCaFT3f6SfbBVCThhcYJ1pk7yNoZgU',
-  authDomain: 'memori-ef27e.firebaseapp.com',
-  databaseURL: 'https://memori-ef27e.firebaseio.com',
-  projectId: 'memori-ef27e',
-  storageBucket: 'memori-ef27e.appspot.com',
-  messagingSenderId: '392826615015'
-}
