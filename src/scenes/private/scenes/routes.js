bb96ba657ab29214cc7e9c269afe2d48234ba597
Thomas Deconinck <thomas.d@colisweb.com>
2018-12-06 17:40:40 +0100

    up

---
+++ src/scenes/private/scenes/routes.js
@@ -0,0 +1,14 @@
+// @flow
+
+export default {
+  anonymous: {
+    home: '/',
+    login: '/login',
+    register: '/register'
+  },
+  authenticated: {
+    home: '/app',
+    manageList: '/app/lists',
+    support: '/app/support'
+  }
+}
